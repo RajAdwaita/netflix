9bfbaf16b2d8ae55d3c94600b1a23f2fc6d7e6a8
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-12-06 12:49:20 +0530

    Add

--- netflix/src/screens/SignUpScreen.jsx
+++ netflix/src/screens/SignUpScreen.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
-
+import './SignUpScreen.css';
 function SignUpScreen() {
     return (
-        <div className='signUpScreen'>SignUpScreen</div>
+        <div className='signupScreen'>
+            <form action="">
+                <input type="email" placeholder='EMAIL' />
+                <input type="password" placeholder='PASSWORD' />
+                <button type="submit" className='signupScreen_button'>SIGN IN</button>
+            </form>
+
+        </div>
     )
 }
 
