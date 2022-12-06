ebdd1daa43e03bbe11706bf499b38700281e8817
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-12-06 10:46:55 +0530

    add

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
+import LoginScreen from "./screens/LoginScreen";
 import {
   BrowserRouter as Router,
   Routes,
@@ -10,11 +11,22 @@ import {
 } from 'react-router-dom';
 
 function App() {
+  const user = null;
   return (
     <div className="app">
       {/* <h1>Lets build</h1> */}
 
       <Router>
+        {!user ? (
+          <LoginScreen />
+        ) : (
+          <Routes>
+
+
+            <Route exact path="/" element={<HomeScreen />} />
+
+          </Routes>
+        )}
 
         {/* <nav>
             <ul>
@@ -30,12 +42,7 @@ function App() {
             </ul>
           </nav> */}
 
-        <Routes>
 
-
-          <Route exact path="/" element={<HomeScreen />} />
-
-        </Routes>
       </Router>
 
     </div >
---
+++ netflix/src/screens/LoginScreen.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './LoginScreen.css';
+function LoginScreen() {
+    return (
+        <div className='loginScreen'>
+            <div className="loginScreen_background">
+                <img className='loginScreen_logo' src="https://townsquare.media/site/442/files/2016/03/netflix-logo-e1458912412513.png" alt="" />
+            </div>
+
+
+        </div>
+    )
+}
+
+export default LoginScreen
