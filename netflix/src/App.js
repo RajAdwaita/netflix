529980551dc677191c57da705410e9da9a52354b
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-12-06 23:14:48 +0530

    Add

--- netflix/src/App.js
+++ netflix/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Counter } from "./features/counter/Counter";
+// import { Counter } from "./features/counter/Counter";
 import "./App.css";
 import HomeScreen from "./screens/HomeScreen";
 import LoginScreen from "./screens/LoginScreen";
@@ -9,9 +9,29 @@ import {
   Route,
 
 } from 'react-router-dom';
+import { useEffect } from "react";
+import { auth } from "./firebase";
 
 function App() {
   const user = null;
+
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+        //Logged in
+        console.log(userAuth);
+      } else {
+        //Logged out
+        // console.log
+
+      }
+    })
+    return unsubscribe;
+
+  }, [])
+
+
   return (
     <div className="app">
       {/* <h1>Lets build</h1> */}
