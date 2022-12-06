7142e094a7f7c5f3476aa142d41939dd231f929b
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-12-06 15:03:40 +0530

    Add

--- netflix/src/firebase.js
+++ netflix/src/firebase.js
@@ -1,3 +1,8 @@
+
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
 const firebaseConfig = {
     apiKey: "AIzaSyBpVDKAIO2R98rSTocwkhWovA0qhETTAnA",
     authDomain: "netflix-91d10.firebaseapp.com",
@@ -6,3 +11,13 @@ const firebaseConfig = {
     messagingSenderId: "943823698081",
     appId: "1:943823698081:web:92955579450df16265d8e0"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+
+export { auth }
+export default db;
+
--- netflix/src/screens/SignUpScreen.jsx
+++ netflix/src/screens/SignUpScreen.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
+import React, { useRef } from 'react'
+import { auth } from '../firebase';
 import './SignUpScreen.css';
 function SignUpScreen() {
+
+    const emailRef = useRef(null);
+    const passwordRef = useRef(null);
+
+
+
+    const register = (e) => {
+        e.preventDefault();
+        auth.createUserWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value
+        ).then((authUser) => {
+            console.log(authUser);
+        }).catch(error => {
+            alert(error.message)
+        });
+
+
+
+    }
+
+    const signIn = (e) => {
+        e.preventDefault();
+
+        auth.signInWithEmailAndPassword(
+            emailRef.current.value,
+            passwordRef.current.value).then((authUser) => {
+                console.log(authUser);
+            }).catch(error => {
+                alert(error.message)
+            });
+    }
+
     return (
         <div className='signupScreen'>
             <form action="">
-                <input type="email" placeholder='EMAIL' />
-                <input type="password" placeholder='PASSWORD' />
-                <button type="submit" className='signupScreen_button'>SIGN IN</button>
+                <input ref={emailRef} type="email" placeholder='EMAIL' />
+                <input ref={passwordRef} type="password" placeholder='PASSWORD' />
+                <button type="submit" className='signupScreen_button' onClick={signIn}>SIGN IN</button>
+
+                <h4> <span className='signupScreen_gray'>New to Netflix?</span>  <span className='signupScreen_link' onClick={register}>Sign up now.</span> </h4>
             </form>
 
         </div>
