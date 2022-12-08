e84db6e99543ced728029e71be1f857327cc1eaa
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-12-08 23:30:45 +0530

    Add

--- netflix/src/Nav.jsx
+++ netflix/src/Nav.jsx
@@ -1,8 +1,9 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useHistory } from 'react'
 import './Nav.css';
 function Nav() {
 
     const [show, handleShow] = useState(false);
+    const history = useHistory();
 
 
     const transitionNavBar = () => {
@@ -28,8 +29,8 @@ function Nav() {
     return (
         <div className={`nav ${show && "nav_black"}`}>
             <div className="nav_contents">
-                <img className='nav_logo' src="https://e7.pngegg.com/pngimages/119/342/png-clipart-logo-netflix-nasdaq-nflx-brand-television-copywriter-floor-television-text.png" alt="" />
-                <img className='nav_avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
+                <img onClick={() => history.push("/")} className='nav_logo' src="https://e7.pngegg.com/pngimages/119/342/png-clipart-logo-netflix-nasdaq-nflx-brand-television-copywriter-floor-television-text.png" alt="" />
+                <img onClick={() => history.push("/profile")} className='nav_avatar' src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="" />
 
             </div>
         </div>
--- netflix/src/screens/ProfileScreen.jsx
+++ netflix/src/screens/ProfileScreen.jsx
@@ -1,10 +1,40 @@
 import React from 'react'
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import Nav from '../Nav';
 import './ProfileScreen.css';
 function ProfileScreen() {
+    const user = useSelector(selectUser);
     return (
         <div className='profileScreen'>
+            <Nav />
 
+            <div className="profileScreen_body">
+                <h1>Edit Profile</h1>
+                <div className="profileScreen_info">
+                    <img
+                        src="https://pbs.twimg.com/profile_images/1240119990411550720/hBEe3tdn_400x400.png"
+                        alt=""
+                    />
+                    <div className="profileScreen_details">
+                        <h2>{user.email}</h2>
+                        <div className="profileScreen_plans">
+                            <h3>Plans</h3>
+                            <button className="profileScreen_plan">Renewal date: 04/03/2021</button>
+                            <button className="profileScreen_plan">Netflix Standard</button>
+                            <button className="profileScreen_plan">Netflix Basic</button>
+                            <button className="profileScreen_plan">Netflix Premium</button>
+                            <button className="profileScreen_signOut">Sign Out</button>
+
+
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
+
     )
 }
 
