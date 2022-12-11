65f38677e4d84afe6a0f2a8ca999353c61011d08
Adwaita Raj Modak <adwaitaraj@gmail.com>
2022-12-11 07:27:19 +0530

    ADD

--- netflix/src/features/userSlice.js
+++ netflix/src/features/userSlice.js
@@ -1,16 +1,8 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 
-
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched. Thunks are
-// typically used to make async requests.
-
-
 export const userSlice = createSlice({
-  name: "user",
+  name: 'user',
   initialState: {
     user: null,
   },
